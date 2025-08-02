b9008903f10f6c29e14c5a45033a345f2f896524
kushalikaaroshi <kushalikaaroshijayaweera@gmail.com>
2025-08-01 20:23:34 -0700

    change

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    function index(){
+
+        return User::all();
+    }
+
+    function register(Request $request ){
+        $request->validate([
+            'name'=> 'required',
+            'email'=> 'required|email|unique:users',
+            'contact_number'=> 'required',
+            'address'=> 'required',
+            'password'=> 'required'
+        ]);
+    }
 }
